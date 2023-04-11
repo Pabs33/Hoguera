c427d7d079543dcef5f8018dd0eaae1085bdcf71
Pablo Martínez Aznar <pablomartinezaznar@gmail.com>
2023-04-11 21:18:04 +0200

    Modifico el script para en un futuro añadirle la opcion de cerrar todos los procesos que genera y cerrar "correctamente" el programa

--- EjecutarApp.sh
+++ EjecutarApp.sh
@@ -2,6 +2,9 @@
 
 #script para poder ejecutar la aplicacion de la loteria de la hoguera 
 
+#Ejecutar el servicio de MySQL
+service MySQL start
+
 #Ejecutar en segundo plano el servidor backend
 cd Back
 npx nodemon index.js&
@@ -14,3 +17,17 @@ cd Front/HogueraFront
 npm start&
 
 echo "servidor FrontEnd ejecutado, espera un poco hasta que este listo y luego accede a localhost:4200 para usar la app"
+
+salir = false
+
+while [!$salir]
+do
+    echo "introduce acabar para cerrar todos los procesos"
+    read acabar
+    if [$acabar = "acabar"]
+    then
+    #TODO buscar que procesos arranca nodemon y angular, buscar su PID y matarlos
+    #TODO Igual es mejor usar python para este script
+    $salir = true
+    fi
+done
